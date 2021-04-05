63ad838c84c881a20ff415e764079e0a32fa9cc2
Farouk Soulé <soule.farouk@aef.info>
2021-04-05 11:42:34 +0200

    Header and all component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ShopComponent } from './shop/shop.component';
+import { ProductsComponent } from './shop/products/products.component';
+import { SingleProductComponent } from './shop/single-product/single-product.component';
+import { CartComponent } from './shop/cart/cart.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+
+export const routes:Routes=[
+  {path:'home', component:HomeComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'shop', component:ShopComponent},
+  {path:'cart', component:CartComponent},
+  {path:'single-product/:id', component:SingleProductComponent},
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+ // {path:'checkout', canActivate:[AuthGuard], component:CheckoutComponent},
+  {path:'category/:id', component:CategoryComponent},
+  {path:'NotFound', component:NotFoundComponent},
+  {path:'products', component:ProductsComponent},
+  {path:'', component:ShopComponent},
+  {path:'**', redirectTo:'NotFound', pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent,
+    ShopComponent,
+    ProductsComponent,
+    SingleProductComponent,
+    CartComponent,
+    NotFoundComponent,
+    CategoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
