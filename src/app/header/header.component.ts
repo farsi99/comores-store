6a0b7a9a319b1149504091c8b7fd25644d30653d
Farouk Soulé <soule.farouk@aef.info>
2021-04-06 08:21:18 +0200

    gestion panier

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from '../model/cart';
+import { CartService } from '../service/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  cartData;
+  cart:Cart[];
+
+  constructor(private cartService:CartService) { }
 
   ngOnInit(): void {
+    this.cartData = this.cartService.cartData;
+    this.cart = this.cartService.cart;
   }
 
 }
